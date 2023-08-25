8855d040cb9e2f9937a98d22a0a89ad9d79c8e5f
gaha20 <128089822+gaha20@users.noreply.github.com>
2023-08-25 03:37:26 +0000

    class Court Added

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User, Sport_center, Center_schedule
+from api.models import db, User, Sport_center
 from api.utils import generate_sitemap, APIException
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_jwt_extended import create_access_token
